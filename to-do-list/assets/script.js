b1bd9afa39430295ea64655c3f566e02fa37d871
pgs96 <pau.gs96@gmail.com>
2024-01-09 21:06:02 -0600

    calendario3

--- to-do-list/assets/script.js
+++ to-do-list/assets/script.js
@@ -27,6 +27,36 @@ const months = [
   "December",
 ];
 
+const eventsArr = [
+  {
+    day: 09,
+    month: 01,
+    year: 2024,
+    events: [
+      {
+        title: "Event 1 lorem opsum dolar sit genfa tersd dsad",
+        time: "10:00 AM",
+      },
+      {
+        title: "Event 2",
+        time: "11:00 AM"
+      }
+    ]
+  },
+  {
+    day: 15,
+    month: 01,
+    year: 2024,
+    events: [
+      {
+        title: "Event 1 lorem opsum dolar sit genfa tersd dsad",
+        time: "10:00 AM",
+      },
+    ]
+  }
+]
+
+
 // <================= CURRENT MONTH =====================>
 // Function to ADD DAYS
 
@@ -52,15 +82,39 @@ function initCalendar() {
 
   // Current month and days
   for (let i = 1; i <= lastDate; i++) {
+    
+    // CHECKS IF EVENT IS ON PRESENT DAY
+    let event = false;
+    eventsArr.forEach((eventObj) => {
+      if (
+        eventObj.day === i &&
+        eventObj.month === month + 1 &&
+        eventObj.year === year
+      ) {
+        // IF AN EXISITING EVENT IS FOUND
+        event = true;
+      }
+    })
+    
+
     if (
       i == new Date().getDate() &&
       year == new Date().getFullYear() &&
       month == new Date().getMonth()
       ) {
-      days += `<div class="day today">${i}</div>`;
+      if (event) {
+        days += `<div class="day today event">${i}</div>`;
+      } else {
+        days += `<div class="day today">${i}</div>`;
+      }
     }
+
     else {
-      days += `<div class="day">${i}</div>`;
+      if (event) {
+        days += `<div class="day event">${i}</div>`;
+      } else {
+        days += `<div class="day">${i}</div>`;
+      }
     }
     }
 
@@ -71,6 +125,7 @@ function initCalendar() {
   }
 
   daysContainer.innerHTML = days;
+  addListener();
 }
 
 initCalendar();
@@ -154,3 +209,97 @@ function gotoDate() {
 }
 
 gotoBtn.addEventListener("click", gotoDate);
+
+
+
+
+// <================= ADD EVENT FUNCTIONS =====================>
+
+
+
+const addEventBtn = document.querySelector(".add-event"),
+  addEventContainer = document.querySelector(".add-event-wrapper"),
+  addEventCloseBtn = document.querySelector(".close"),
+  addEventTitle = document.querySelector(".event-name"),
+  addEventFrom = document.querySelector(".event-time-from"),
+  addEventTo = document.querySelector(".event-time-to");
+
+  addEventBtn.addEventListener("click", () => {
+    addEventContainer.classList.toggle("active");
+  });
+  addEventCloseBtn.addEventListener("click", () => {
+    addEventContainer.classList.remove("active");
+  });
+
+  document.addEventListener("click", (e) => {
+    if(e.target !== addEventBtn && !addEventContainer.contains(e.target)) {
+      addEventContainer.classList.remove("active");
+    }
+  });
+
+  // ALLOWS ONLY A MAXIMUM OF 50 CHARACTERS
+
+  addEventTitle.addEventListener("input", (e) => {
+    addEventTitle.value = addEventTitle.value.slice(0, 50);
+  });
+
+  // TIME FORMAT
+
+  addEventFrom.addEventListener("input", (e) => {
+    // ONLY ALLOWS NUMERICAL VALUES
+    addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
+    // ADDS THE ':' AUTOMATICALLY
+    if (addEventFrom.value.length === 2) {
+      addEventFrom.value += ":";
+    }
+    // NO MORE THAN 5 CHARACTERS
+    if (addEventFrom.value,length > 5) {
+      addEventFrom.value = addEventFrom.value.slice(0, 5);
+    }
+  });
+
+  addEventTo.addEventListener("input", (e) => {
+  // ONLY ALLOWS NUMERICAL VALUES
+    addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, "");
+  // ADDS THE ':' AUTOMATICALLY
+    if (addEventTo.value.length === 2) {
+    addEventTo.value += ":";
+   }
+  // NO MORE THAN 5 CHARACTERS
+    if (addEventTo.value, length > 5) {
+    addEventTo.value = addEventTo.value.slice(0, 5);
+   }
+  });
+
+
+
+  // ADD LISTENER ON DAYS
+
+  function addListener() {
+    const days = document.querySelectorAll(".day");
+    days.forEach((day) => {
+      day.addEventListener("click", (e) => {
+        activeDay = Number(e.target.innerHTML);
+
+        days.forEach((day) => {
+          day.classList.remove("active");
+        });
+        if (e.target.classList.contains("prev-date")) {
+          prevMonth();
+
+          setTimeout(() => {
+            const days = document.querySelectorAll(".day");
+            days.forEach((day)=> {
+              if (
+                !day.classList.contains("prev-date") &&
+                day.innerHTML === e.target.innerHTML
+                ) {
+                  day.classList.add("active");
+                }
+            });
+          });
+        }
+
+      });
+    });
+  }
